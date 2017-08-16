f6cc3efe932d6d640c5991bea2cc2dd08e6099e6
Ray Paragas <rayy.paragas@gmail.com>
2017-08-16 14:44:31 +1000

    new commit

---
+++ Multiple Choice Engine/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Multiple Choice Engine
+//
+//  Created by Ray Paragas on 16/8/17.
+//  Copyright © 2017 Ray Paragas. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            if error != nil {
+                print("Error")
+            } else {
+                print("Successful Signin")
+                self.performSegue(withIdentifier: "successLoginSegue", sender: nil)
+            }
+        })
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-Multiple Choice Engine/Pods-Multiple Choice Engine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Multiple_Choice_Engine : NSObject
+@end
+@implementation PodsDummy_Pods_Multiple_Choice_Engine
+@end
